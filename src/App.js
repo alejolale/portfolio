86bcff89eb4e9eaaa60d0fd37c776bdabe5a174f
alejolale <sergio.prieto01@outlook.com>
2020-02-20 23:25:27 +0100

    Add i18n files but not implemented

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import './App.css';
+//import { I18nextProvider } from 'react-i18next';
+//import i18n from './i18n/index';
+
+import Presentation from './components/presentation/Presentation';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi I'm Sergio Prieto</h1>
-      <h2>Full-stack web developper in javascript</h2>
+      {/* <I18nextProvider i18n={i18n}>
+        <Presentation />
+      </I18nextProvider> */}
+      <Presentation />
     </div>
   );
 }
---
+++ src/components/presentation/Presentation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Presentation = () => {
+    return(
+        <div>
+            <h1>Hi I'm Sergio Prieto</h1>
+            <h2>Full-stack web developper in javascript</h2>
+        </div>
+    );
+
+}
+
+export default Presentation
